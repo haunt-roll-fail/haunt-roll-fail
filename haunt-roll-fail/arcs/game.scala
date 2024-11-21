6354ac04a2e682f7d54230699307d3bca310821c
Le Yang <le.yang@gmail.com>
2024-11-21 15:44:41 +0000

    use DeckLocation

--- haunt-roll-fail/arcs/game.scala
+++ haunt-roll-fail/arcs/game.scala
@@ -162,6 +162,7 @@ case class Hand(faction : Faction) extends DeckCardLocation
 case class Played(faction : Faction) extends DeckCardLocation
 case class Blind(faction : Faction) extends DeckCardLocation
 case object Deck extends DeckCardLocation
+case object DeckDiscard extends DeckCardLocation
 
 trait CourtLocation
 case object CourtDeck extends CourtLocation
@@ -885,10 +886,10 @@ class Game(val setup : $[Faction], val options : $[Meta.O]) extends BaseGame wit
     implicit val figures = new IdentityTracker[Region, Figure]
 
     val deck = cards.register(Deck, content = DeckCards.deck.%(d => factions.num == 4 || (d.strength > 1 && d.strength < 7)))
+    val disdeck = cards.register(DeckDiscard)
     val court = courtiers.register(CourtDeck, content = CourtCards.deck)
     val market = courtiers.register(CourtMarket)
     val discourt = courtiers.register(CourtDiscard)
-    var disaction : $[DeckCard] = $
 
     var turn : Int = 0
     var chapter : Int = 0
@@ -2576,10 +2577,8 @@ class Game(val setup : $[Faction], val options : $[Meta.O]) extends BaseGame wit
                 factions = factions.dropWhile(_ != current) ++ factions.takeWhile(_ != current)
 
                 factions.foreach { f =>
-                    disaction ++= f.played
-                    f.played --> deck
+                    f.played --> disdeck
                     f.blind --> deck
-
                 }
 
                 lead = None
@@ -2595,7 +2594,7 @@ class Game(val setup : $[Faction], val options : $[Meta.O]) extends BaseGame wit
                 factions.foreach { f =>
                     f.hand --> deck
                 }
-                disaction = $
+                disdeck --> deck
 
                 $(Tycoon, Tyrant, Warlord, Keeper, Empath).foreach { ambition =>
                     if (declared.contains(ambition)) {
