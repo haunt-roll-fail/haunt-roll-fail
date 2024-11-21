c8ca3e99a6bb75a6a0c7ef89cf3534071d0e4015
Le Yang <le.yang@gmail.com>
2024-11-21 07:34:13 +0000

    poc

--- haunt-roll-fail/arcs/game.scala
+++ haunt-roll-fail/arcs/game.scala
@@ -162,7 +162,6 @@ case class Hand(faction : Faction) extends DeckCardLocation
 case class Played(faction : Faction) extends DeckCardLocation
 case class Blind(faction : Faction) extends DeckCardLocation
 case object Deck extends DeckCardLocation
-case object DeckDiscard extends DeckCardLocation
 
 trait CourtLocation
 case object CourtDeck extends CourtLocation
@@ -886,10 +885,10 @@ class Game(val setup : $[Faction], val options : $[Meta.O]) extends BaseGame wit
     implicit val figures = new IdentityTracker[Region, Figure]
 
     val deck = cards.register(Deck, content = DeckCards.deck.%(d => factions.num == 4 || (d.strength > 1 && d.strength < 7)))
-    val disaction = cards.register(DeckDiscard)
     val court = courtiers.register(CourtDeck, content = CourtCards.deck)
     val market = courtiers.register(CourtMarket)
     val discourt = courtiers.register(CourtDiscard)
+    var disaction : $[DeckCard] = $
 
     var turn : Int = 0
     var chapter : Int = 0
@@ -2537,10 +2536,6 @@ class Game(val setup : $[Faction], val options : $[Meta.O]) extends BaseGame wit
 
                     f.taking = $
 
-                    f.played.foreach { p =>
-                        p --> game.disaction
-                    }
-
                     f.discardAfterRound.foreach { c =>
                         c --> discourt
 
@@ -2581,8 +2576,10 @@ class Game(val setup : $[Faction], val options : $[Meta.O]) extends BaseGame wit
                 factions = factions.dropWhile(_ != current) ++ factions.takeWhile(_ != current)
 
                 factions.foreach { f =>
+                    disaction ++= f.played
                     f.played --> deck
                     f.blind --> deck
+
                 }
 
                 lead = None
@@ -2598,6 +2595,7 @@ class Game(val setup : $[Faction], val options : $[Meta.O]) extends BaseGame wit
                 factions.foreach { f =>
                     f.hand --> deck
                 }
+                disaction = $
 
                 $(Tycoon, Tyrant, Warlord, Keeper, Empath).foreach { ambition =>
                     if (declared.contains(ambition)) {
