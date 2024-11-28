9540572c8d7d577d74af7f402fd730654dc76b97
Le Yang <le.yang@gmail.com>
2024-11-28 14:56:43 +0000

    refactor

--- haunt-roll-fail/arcs/game.scala
+++ haunt-roll-fail/arcs/game.scala
@@ -1973,6 +1973,7 @@ class Game(val setup : $[Faction], val options : $[Meta.O]) extends BaseGame wit
                     .each(market.%(c => Influence(c).exists(_.faction == e)))(c => RansackAction(f, c, then).as(c))
                     .needOk
                     .bail(then)
+
             case RansackAction(f, c, then) =>
                 market --> c --> f.loyal
 
